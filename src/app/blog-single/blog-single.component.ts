bf0aa0904b5ddba448724f3953170575965af8e0
Elie Simpore <simporeelie97@gmail.com>
2024-07-31 00:41:46 +0000

    Nouveauprojet

---
+++ src/app/blog-single/blog-single.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-single',
+  standalone: true,
+  imports: [],
+  templateUrl: './blog-single.component.html',
+  styleUrl: './blog-single.component.css'
+})
+export class BlogSingleComponent {
+
+}
